0c862401a8685612c7e97bc0723960de115d733b
Kawishva32 <111407288+Kawishva@users.noreply.github.com>
2024-04-23 14:37:13 +0530

    23/04/2024

--- app/src/main/java/com/example/githmidiaryapp/Main_menue_interface.java
+++ app/src/main/java/com/example/githmidiaryapp/Main_menue_interface.java
@@ -3,18 +3,23 @@ package com.example.githmidiaryapp;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Random;
 
 
 public class Main_menue_interface extends AppCompatActivity {
 
     Button view_diary_btn_id,add_entry_btn_id,settings_btn_id;
     TextView welcome_text_id;
+    ImageView user_image_id;
     SharedPreferences sharedPreferences;
     static String welcome_text_user_name;
 
@@ -29,10 +34,12 @@ public class Main_menue_interface extends AppCompatActivity {
         add_entry_btn_id = findViewById(R.id.add_entry_btn_id);
         settings_btn_id = findViewById(R.id.settings_btn_id);
         welcome_text_id = findViewById(R.id.welcome_text_id);
+        user_image_id = findViewById(R.id.user_image_id);
 
         sharedPreferences = getSharedPreferences("USER_DATA",MODE_PRIVATE);
 
         change_user_name_and_title();
+        random_image();
 
 
         view_diary_btn_id.setOnClickListener(new View.OnClickListener() {
@@ -71,13 +78,23 @@ public class Main_menue_interface extends AppCompatActivity {
         change_user_name_and_title();
     }
 
+    void random_image(){
+
+        Random random = new Random();// Create an instance of Random class
+
+        int randomNumber = random.nextInt(5) + 1; // Generate a random integer from 1 to 5
+
+        String drawableName = "pb" + randomNumber; // Construct the drawable name
+        int resourceId = getResources().getIdentifier(drawableName, "drawable", getPackageName());// Get the resource identifier for the drawable
+        user_image_id.setImageResource(resourceId);// Set the drawable to the ImageView
+    }
+
     void change_user_name_and_title(){
         welcome_text_user_name = sharedPreferences.getString("User_name","");
         if(welcome_text_user_name.isEmpty()){
             welcome_text_id.setText("Welcome\t!");
         }else {
-
-            welcome_text_id.setText("Welcome\t"+welcome_text_user_name+"\t!");
+            welcome_text_id.setText("Welcome\t "+welcome_text_user_name+ " \t!");
         }
     }
 }
